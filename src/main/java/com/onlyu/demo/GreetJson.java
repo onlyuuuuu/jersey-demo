682cec30ecdbe4f1dc4874b9be24d07fa5edc2fb
Anh Nguyễn <duyanhnguyenngoc97@gmail.com>
2020-08-01 11:46:59 +0700

    .

--- src/main/java/com/onlyu/demo/GreetJson.java
+++ src/main/java/com/onlyu/demo/GreetJson.java
@@ -5,12 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 import java.util.List;
 
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class GreetJson {
